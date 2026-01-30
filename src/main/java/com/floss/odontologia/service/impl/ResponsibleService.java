2747822a6bfd646255b879568e02ad4f529d51ed
matias-devv <rmatias.dev@gmail.com>
2026-01-30 13:20:57 -0300

    refactor: fix typo in ResponsibleRepository and update PaymentReceipt naming
    - Rename IReponsibleRepository to IResponsibleRepository. - Rename IPaymentReceipt to IPaymentReceiptRepository for consistency.

--- src/main/java/com/floss/odontologia/service/impl/ResponsibleService.java
+++ src/main/java/com/floss/odontologia/service/impl/ResponsibleService.java
@@ -1,7 +1,7 @@
 package com.floss.odontologia.service.impl;
 
 import com.floss.odontologia.model.ResponsibleAdult;
-import com.floss.odontologia.repository.IReponsibleRepository;
+import com.floss.odontologia.repository.IResponsibleRepository;
 import com.floss.odontologia.service.interfaces.IResponsibleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 public class ResponsibleService implements IResponsibleService {
 
     @Autowired
-    private IReponsibleRepository iResponsibleRepository;
+    private IResponsibleRepository iResponsibleRepository;
 
     @Override
     public String createResponsible(ResponsibleAdult responsible) {
