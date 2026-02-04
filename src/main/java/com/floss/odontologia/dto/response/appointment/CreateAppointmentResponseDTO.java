f6a6e8365972362351307b6c7643770e2d5064a7
matias-devv <rmatias.dev@gmail.com>
2026-02-04 11:34:55 -0300

    feat(appointment-dto): add CreateAppointmentResponseDTO with appointment and payment details

--- src/main/java/com/floss/odontologia/dto/response/appointment/CreateAppointmentResponseDTO.java
+++ src/main/java/com/floss/odontologia/dto/response/appointment/CreateAppointmentResponseDTO.java
@@ -1,12 +1,34 @@
 package com.floss.odontologia.dto.response.appointment;
 
 import com.floss.odontologia.enums.AppointmentStatus;
+import com.floss.odontologia.enums.PaymentMethod;
+import com.floss.odontologia.enums.PaymentStatus;
 import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Builder
 public record CreateAppointmentResponseDTO(Long id_appointment,
                                            Long id_treatment,
                                            Long id_pay,
-                                           String pay_link, //only if PaymentMethod = MERCADO_PAGO
-                                           AppointmentStatus appointment_status) {
+
+                                           // Appointment info
+                                           LocalDate date,
+                                           LocalTime start_time,
+                                           Integer duration_minutes,
+                                           LocalTime end_time,
+
+                                           // Payment info
+                                           BigDecimal amount_to_pay,
+                                           PaymentMethod payment_method,
+                                           String payment_link,
+
+                                           // Status
+                                           AppointmentStatus appointment_status,
+                                           PaymentStatus payment_status,
+
+                                           // Product info
+                                           String product_name) {
 }
