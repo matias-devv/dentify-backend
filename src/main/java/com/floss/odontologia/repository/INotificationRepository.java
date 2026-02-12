e954aef6e75725a1a6b00cbd05f6dea03cf8c076
matias-devv <rmatias.dev@gmail.com>
2026-02-11 22:22:52 -0300

    feat: add Notification and PatientStat repositories

--- src/main/java/com/floss/odontologia/repository/INotificationRepository.java
+++ src/main/java/com/floss/odontologia/repository/INotificationRepository.java
@@ -1,9 +1,16 @@
 package com.floss.odontologia.repository;
 
+import com.floss.odontologia.enums.NotificationChannel;
+import com.floss.odontologia.enums.NotificationType;
+import com.floss.odontologia.enums.ReminderWindow;
+import com.floss.odontologia.model.Appointment;
 import com.floss.odontologia.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface INotificationRepository extends JpaRepository<Notification, Long> {
+
 }
--- src/main/java/com/floss/odontologia/repository/IPatientStatRepository.java
+++ src/main/java/com/floss/odontologia/repository/IPatientStatRepository.java
@@ -1,4 +1,9 @@
 package com.floss.odontologia.repository;
 
-public class IPatientStatRepository {
+import com.floss.odontologia.model.PatientStat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPatientStatRepository extends JpaRepository<PatientStat, Long> {
 }
