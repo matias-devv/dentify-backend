d08597248cb42ba3bbba77a9a6326e16a1f4ad33
matias-devv <rmatias.dev@gmail.com>
2026-02-11 22:57:10 -0300

    feat(cloudinary): add Cloudinary service interface for PDF upload and deletion

--- src/main/java/com/floss/odontologia/service/interfaces/ICloudinaryService.java
+++ src/main/java/com/floss/odontologia/service/interfaces/ICloudinaryService.java
@@ -1,4 +1,15 @@
 package com.floss.odontologia.service.interfaces;
 
 public interface ICloudinaryService {
+
+    /**
+     * Upload PDF to Cloudinary
+     */
+    String uploadPDF(String filename, byte[] fileBytes);
+
+    /**
+     * Delete file by filename
+     */
+    void delete(String filename);
+
 }
